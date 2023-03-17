58c9f43946c33ae566c2ffea08eac40512281269
Atilla <109938381+atillaertas1@users.noreply.github.com>
2023-03-18 01:47:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const container = document.querySelector(".container");
+const filmAfis = document.querySelector(".film-afis img");
+const filmAfis2 = document.querySelector(".fatura img");
+const fiyat = document.querySelector('.screen span');
+const select = document.querySelector(".movie-list select");
+const movie = document.getElementById("movie")
+
+imageControl()
+
+container.addEventListener("click",function(e){
+    //console.log(e.target);
+
+    if(e.target.classList.contains("seat") && !e.target.classList.contains("filled")){
+        //console.log("ok")
+        e.target.classList.toggle("selected");
+    }
+    calculatePrice();
+});
+
+select.addEventListener("change",function(){
+
+    calculatePrice();
+    imageControl()
+});
+
+function calculatePrice()
+{
+    // console.log(select.value)
+    let seatCount = document.querySelectorAll(".seat.selected").length - 1;
+    let price = movie.value;
+
+    fiyat.innerHTML = price * seatCount;
+
+    // console.log(seatCount);
+    // console.log(price);
+}
+
+function imageControl()
+{
+    if(movie.value == 30)
+    {
+        filmAfis.src = "masumiyet.png";
+        filmAfis2.src = "masumiyet.png";
+        // console.log(select.value)
+    }
+    else if(movie.value == 40)
+    {
+        filmAfis.src = "ruya.png";
+        filmAfis2.src = "ruya.png";
+
+    }
+    else if(movie.value == 50)
+    {
+        filmAfis.src = "lotr.png";
+        filmAfis2.src = "lotr.png";
+ 
+    }
+}
